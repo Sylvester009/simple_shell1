3d653ba0177f095d4f08674d560324b1f3d77793
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-03 16:38:14 +0100

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -2,14 +2,34 @@
 
 void tokenize(char *input, char *args[])
 {
-    /** Use strtok to split the input into tokens*/
-    char *token = strtok(input, " ");
     int i = 0;
+    int start = 0;
+    int end = 0;
+    int token_length = 0;
 
-    while (token != NULL && i < MAX_SIZE - 1)
+    while (input[start] != '\0' && i < MAX_SIZE - 1)
     {
-        args[i++] = token;
-        token = strtok(NULL, " ");
+        while (input[start] == ' ')
+        {
+            start++;
+        }
+
+        /** Find the end of the current token*/
+        end = start;
+        while (input[end] != '\0' && input[end] != ' ')
+        {
+            end++;
+        }
+
+        /** Extract the token and add it to args*/
+        token_length = end - start;
+        args[i] = malloc(token_length + 1);
+        strncpy(args[i], input + start, token_length);
+        args[i][token_length] = '\0';
+
+        /** Move to the next token*/
+        start = end;
+        i++;
     }
 
     args[i] = NULL;
