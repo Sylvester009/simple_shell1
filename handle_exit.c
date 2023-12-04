a359e47a1601536f6990bf57a16d275b1a43a9d7
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-05 00:34:07 +0100

    Update handle_exit.c

--- handle_exit.c
+++ handle_exit.c
@@ -1,7 +1,40 @@
 #include "main.h"
 
-void handle_exit(int status)
-{
-    printf("Exiting the shell\n");
-    exit(status);
+int exit_shell() {
+    int exit_status;
+    int input_valid;
+
+    do {
+        input_valid = 1;  /* Assume input is valid */
+
+        /** Get the exit status from the user*/
+        printf("Enter exit status: ");
+        fflush(stdout);
+
+        char *exit_input = my_getline();
+
+        if (!exit_input) {
+            printf("\n");
+            break;
+        }
+
+        if (exit_input[0] == '\0') {
+            /** If no exit status provided, exit with default status*/
+            exit(EXIT_SUCCESS);
+        }
+
+        exit_status = str_integer(exit_input);
+        if (exit_status == -1) {
+            printf("Illegal number: %s\n", exit_input);
+            input_valid = 0;  /* Invalid input, repeat the loop */
+        } else {
+            /** Exit with the specified status*/
+            exit(exit_status);
+        }
+
+        free(exit_input);
+
+    } while (!input_valid);
+
+    return -2;
 }
