f447eff5745ab22c7328c74702bafc71cce3804b
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-05 00:01:03 +0100

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -1,35 +1,20 @@
 #include "main.h"
 
-void tokenize(char *input, char *args[])
-{
+void tokenize(char *input, char *args[]) {
     int i = 0;
-    int start = 0;
-    int end = 0;
-    int token_length = 0;
+    char *token = input;
 
-    while (input[start] != '\0' && i < MAX_SIZE - 1)
-    {
-        while (input[start] == ' ' || input[start] == '\t')
-        {
-            start++;
+    while (*token != '\0') {
+        while (*token == ' ' || *token == '\t' || *token == '\n') {
+            *token = '\0';
+            token++;
         }
 
-        /** Find the end of the current token*/
-        end = start;
-        while (input[end] != '\0' && input[end] != ' ' && input[end] != '\t')
-        {
-            end++;
+        args[i++] = token;
+
+        while (*token != '\0' && *token != ' ' && *token != '\t' && *token != '\n') {
+            token++;
         }
-
-        /** Extract the token and add it to args*/
-        token_length = end - start;
-        args[i] = malloc(token_length + 1);
-        strncpy(args[i], input + start, token_length);
-        args[i][token_length] = '\0';
-
-        /** Move to the next token*/
-        start = end;
-        i++;
     }
 
     args[i] = NULL;
