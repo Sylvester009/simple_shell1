1a8d3604874bd25a3b07944461779dbeb6c9032c
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-05 00:17:04 +0100

    Update and rename getline.txt to getline.c

--- getline.c
+++ getline.c
@@ -2,38 +2,38 @@
 
 char *my_getline(void) {
     static char buffer[MAX_SIZE];
-    size_t buffer_index = 0;
-    size_t buffer_size = 0;
-    size_t line_length = 0;
+    static size_t buffer_index = 0;
+    static size_t buffer_size = 0;
 
-     char *line = malloc(line_length + 1);
+    char *line = malloc(line_length + 1);
 
-    /** Check if buffer needs to be filled */
+    /**Check if the buffer needs to be filled*/
     if (buffer_index >= buffer_size) {
         buffer_size = fread(buffer, 1, MAX_SIZE, stdin);
+
         if (buffer_size == 0) {
-            /** End of file or an error occurred*/
-            return (NULL);
+            return NULL;
         }
+
         buffer_index = 0;
     }
 
-    /** Find the length of the line*/
+    
+    size_t line_length = 0;
     while (buffer_index + line_length < buffer_size && buffer[buffer_index + line_length] != '\n') {
         line_length++;
     }
 
-    /** Allocate memory for the line*/
     if (line == NULL) {
         fprintf(stderr, "Memory allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    /* Copy the line from the buffer to the allocated memory*/
+    
     memcpy(line, buffer + buffer_index, line_length);
     line[line_length] = '\0';
 
-    /** Move the buffer index to the next line*/
+    
     buffer_index += line_length + 1;
 
     return (line);
