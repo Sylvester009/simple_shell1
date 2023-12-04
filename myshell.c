befa37df2ce64eb8541d6ed26c720abe4ccd5485
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-05 00:06:19 +0100

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -2,54 +2,43 @@
 
 void prompt();
 
-int main(void)
-{
-    char *input = NULL; 
+int main(void) {
+    char *input = NULL;
     size_t len;
     pid_t pid;
     int status;
 
-    char *args[MAX_SIZE]; /** Assuming a maximum number of arguments */
+    char *args[MAX_SIZE];
 
     char *env[] = {NULL};
 
-    while (1)
-    {
+    while (1) {
         prompt();
 
-        if (getline(&input, &len, stdin) == -1)
-        {
-            
+        if (getline(&input, &len, stdin) == -1) {
             free(input);
             printf("\n");
             break;
         }
 
-        if (len > 0 && input[len - 1] == '\n')
-        {
+        if (len > 0 && input[len - 1] == '\n') {
             input[len - 1] = '\0';
         }
 
         pid = fork();
 
-        if (pid == -1)
-        {
+        if (pid == -1) {
             perror("fork");
             free(input);
             exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
+        } else if (pid == 0) {
             /** Child process */
             tokenize(input, args);
 
-            if (strcmp(args[0], "exit") == 0)
-            {
+            if (strcmp(args[0], "exit") == 0) {
                 free(input);
                 handle_exit(0);
-            }
-            else if (strcmp(args[0], "env") == 0)
-            {
+            } else if (strcmp(args[0], "env") == 0) {
                 free(input);
                 handle_env();
             }
@@ -60,31 +49,25 @@ int main(void)
             perror("execve");
             free(input);
             exit(EXIT_FAILURE);
-        }
-        else
-        {
+        } else {
             /** Parent process */
             waitpid(pid, &status, 0);
 
-            if (WIFEXITED(status))
-            {
+            if (WIFEXITED(status)) {
                 /** Child process terminated normally */
                 printf("%d\n", WEXITSTATUS(status));
-            }
-            else if (WIFSIGNALED(status))
-            {
+            } else if (WIFSIGNALED(status)) {
                 /** Child process terminated by a signal */
                 printf(" %d\n", WTERMSIG(status));
             }
         }
     }
 
-    free(input); 
+    free(input);
     return 0;
 }
 
-void prompt()
-{
+void prompt() {
     printf("$ ");
     fflush(stdout);
 }
