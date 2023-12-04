92ed512028f589d8015ed16681288db5272643fd
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-04 23:53:33 +0100

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
-int main(void) {
+void prompt();
+
+int main(void)
+{
     char input[MAX_SIZE];
     size_t len;
     pid_t pid;
@@ -10,50 +13,72 @@ int main(void) {
 
     char *env[] = {NULL};
 
-    while (1) {
-        printf("$ ");  /** Print prompt*/
-        fflush(stdout);
+    while (1)
+    {
+        prompt();
 
-
-        if (fgets(input, sizeof(input), stdin) == NULL) {
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
             printf("\n");
             break;
         }
 
         len = strlen(input);
-        if (len > 0 && input[len - 1] == '\n') {
+        if (len > 0 && input[len - 1] == '\n')
+        {
             input[len - 1] = '\0';
         }
 
         pid = fork();
 
-        if (pid == -1) {
+        if (pid == -1)
+        {
             perror("fork");
             exit(EXIT_FAILURE);
-        } else if (pid == 0) {
+        }
+        else if (pid == 0)
+        {
             /** Child process */
             tokenize(input, args);
+
+            if (strcmp(args[0], "exit") == 0)
+            {
+                handle_exit(0);
+            }
+            else if (strcmp(args[0], "env") == 0)
+            {
+                handle_env();
+            }
+
             execve(args[0], args, env);
 
             /** If execve fails */
             perror("execve");
             exit(EXIT_FAILURE);
-        } else {
+        }
+        else
+        {
             /** Parent process */
             waitpid(pid, &status, 0);
 
-            if (WIFEXITED(status)) {
+            if (WIFEXITED(status))
+            {
                 /** Child process terminated normally */
-                printf("Child process exited with status %d\n", WEXITSTATUS(status));
-            } else if (WIFSIGNALED(status)) {
+                printf("%d\n", WEXITSTATUS(status));
+            }
+            else if (WIFSIGNALED(status))
+            {
                 /** Child process terminated by a signal */
-                printf("Child process terminated by signal %d\n", WTERMSIG(status));
+                printf(" %d\n", WTERMSIG(status));
             }
         }
-
-        /** Free the memory allocated by my_getline*/
-        free(input);
     }
 
     return 0;
+}
+
+void prompt()
+{
+    printf("$ ");
+    fflush(stdout);
 }
