850303da9ff59e3f18e47b51f7a7ec7f0d227d6b
Samuel Sylvester <samsylvester09@gmail.com>
2023-12-08 02:46:08 +0100

    Update main.h

--- main.h
+++ main.h
@@ -10,12 +10,21 @@
 
 #define MAX_SIZE 1024
 
+typedef struct list_node {
+    char *str;                 
+    struct list_node *next;    
+} list_t;
+
 void tokenize(char *input, char *args[]);
 int exit_shell();
 void handle_env();
 char *my_getline(void);
-char **get_environment(inform_t *inform);
 int _unsetenv(inform_t *inform, char *var);
 int _setenv(inform_t *inform, char *var, char *value);
+
+list_t *create_node(char *str);
+void add_node(list_t **head, char *str);
+void print_list(list_t *head);
+void free_list(list_t *head);
 
 #endif
